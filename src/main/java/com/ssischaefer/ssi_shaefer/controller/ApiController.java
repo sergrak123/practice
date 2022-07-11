8f528d9c527f6ef71c38c45b70057524815f3958
Sergey <sergey-707@mail.ru>
2022-07-11 04:42:22 +0400

    Add RestContoller

--- src/main/java/com/ssischaefer/ssi_shaefer/controller/ApiController.java
+++ src/main/java/com/ssischaefer/ssi_shaefer/controller/ApiController.java
@@ -1,13 +1,29 @@
 package com.ssischaefer.ssi_shaefer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-    @GetMapping("/greeting")
-    public String greeting() {
-        return "Rest Api";
+    @PostMapping("/notification")
+    public  void notificationPointPassed(String tuName,String rpName,String movementType){
+        //
+    }
+    @PostMapping("/printingCompletion")
+    public  void printingCompletion(String tuName, String printerName, String labelTemplate){
+        //
+    }
+
+    @GetMapping("/getTo")
+    public String getTo(String tuName) {
+        //
+        return "(...->rpName)";
+    }
+    @GetMapping("/getLabelTemplate")
+    public String getLabelTemplate(String printerName, String tuName) {
+        //
+        return "(...->labelTemplate)";
     }
 }
