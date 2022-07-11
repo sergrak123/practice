ffe55e6ad733c2f6932427ee6f5036d33ef42e14
Sergey <sergey-707@mail.ru>
2022-07-11 04:09:03 +0400

    Init project

---
+++ src/main/java/com/ssischaefer/ssi_shaefer/SsiShaeferApplication.java
@@ -0,0 +1,13 @@
+package com.ssischaefer.ssi_shaefer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiShaeferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiShaeferApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ssischaefer/ssi_shaefer/entity/TransportUnitEntity.java
@@ -0,0 +1,40 @@
+package com.ssischaefer.ssi_shaefer.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class TransportUnitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TransportUnitEntity() {
+
+    }
+    public TransportUnitEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
